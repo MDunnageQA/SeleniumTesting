a241d5ce232b7b4776c74cb1d08e626ce03d43d3
MDunnageQA <matthew.dunnage.qa@gmail.com>
2020-04-29 09:45:39 +0100

    Made a new similar test

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -44,6 +44,48 @@ public class AppTest
         assertTrue(imagesLink.isDisplayed());
     }
 
+    @Test
+    public void seleniumCatPicsTest() throws InterruptedException
+    {
+        driver.manage().window().maximize();
+        sleep(1000);
+        driver.get("http://google.co.uk");
+        sleep(1000);
+        WebElement googleSearchBar = driver.findElement(By.name("q"));
+        googleSearchBar.sendKeys("cat pics");
+        sleep(1000);
+        googleSearchBar.submit();
+        sleep(1000);
+        WebElement linkToCatPics = driver.findElement(By.partialLinkText("Images for cat"));
+        linkToCatPics.click();
+        sleep(1000);
+        WebElement catPic = driver.findElement(By.className("tx8vtf"));
+        catPic.click();
+        sleep(1000);
+        WebElement catPicBig = driver.findElement(By.className("n3VNCb"));
+        catPicBig.click();
+        sleep(4000);
+    }
+
+    @Test
+    public void loginToBitCoinMine() throws InterruptedException
+    {
+        driver.manage().window().maximize();
+        sleep(1000);
+        driver.get("http://thedemosite.co.uk/login.php");
+        sleep(1000);
+        WebElement userName = driver.findElement(By.name("username"));
+        userName.sendKeys("DunnageBay");
+        sleep(1000);
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys(("Chungus"));
+        sleep(1000);
+        WebElement successMessage = driver.findElement(By.tagName("b"));
+        sleep(1000);
+
+
+    }
+
     @After
     public void tearDown()
     {
